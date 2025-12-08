1d49b1bb61619e9eeb5435dc7d01201221359cb2
Amit Prasad <amit@modal.com>
2025-12-08 18:44:31 +0000

    Timeout and retry connect requests.

--- cmd/connect.go
+++ cmd/connect.go
@@ -121,17 +121,34 @@ func runConnect(cmd *cobra.Command, args []string) error {
 		unhealthy_loop:
 			for {
 				// currently in an unhealthy state
-				err = client.Connect()
-				if err == nil {
-					log.Println("Reconnected...")
-					break unhealthy_loop
-				}
+				connectResult := make(chan error, 1)
+				go func() { connectResult <- client.Connect() }()
+
+				timer := time.NewTimer(reconnectInterval)
 
 				select {
+				case err = <-connectResult:
+					if err == nil {
+						timer.Stop()
+						log.Println("Reconnected...")
+						break unhealthy_loop
+					}
+					log.Printf("Failed to reconnect: %v", err)
+					// Wait for the remaining interval
+					select {
+					case <-timer.C:
+						continue unhealthy_loop
+					case <-ctx.Done():
+						log.Println("Context is Done; received SIGINT or SIGTERM. Breaking out of unhealthy_loop.")
+						break unhealthy_loop
+					}
+				case <-timer.C:
+					log.Println("Reconnect timed out. Retrying...")
+					continue unhealthy_loop
 				case <-ctx.Done():
+					timer.Stop()
 					log.Println("Context is Done; received SIGINT or SIGTERM. Breaking out of unhealthy_loop.")
 					break unhealthy_loop
-				case <-time.After(reconnectInterval):
 				}
 			}
 		}
